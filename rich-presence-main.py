0d24b670c199059f4833307fc5789a83e8833e82
Tim Kiendl <tim.kiendl@acp.de>
2025-07-18 04:19:59 +0200

    peak rank + icon implementiert

--- rich-presence-main.py
+++ rich-presence-main.py
@@ -1,37 +1,35 @@
 import time
-
-import requests  # Needs requests module installed: pip install requests -U
 from pypresence import Presence
 import api.api as api
 
-CLIENT_ID = '1395545885349515274'  # Your client ID here
+CLIENT_ID = '1395545885349515274'
 
-"""
-You need to upload your image(s) here:
-https://discord.com/developers/applications/<APP ID>/rich-presence/assets
-"""
+RPC = Presence(client_id=CLIENT_ID)
+RPC.connect()
 
 def get_presence_data():
     try:
-        rank = api.main()
+        rank = api.main()  # z.B. "Gold 3"
         rank_name, rank_val = rank.split(' ')
-        image = rank_name.lower() + "-" + rank_val.lower() + ".png"
-        image = "static/"+ image
+        image = rank_name.lower() + "-" + rank_val.lower()
 
-        return {'state': "Peak Rank: " + rank, 'small_image':image}
-    except Exception:
-        # update failed data
-        return {'state': 'Error!'}
-
+        return {
+            'state': "Peak Rank: " + rank,
+            'small_image': image
+        }
+    except Exception as e:
+        print(f"[ERROR] {e}")
+        return {
+            'state': 'Error!',
+            'small_image': 'error',  # Optional: Fallback-Bild
+            'small_text': "Something went wrong"
+        }
 
 def run():
-    presence = Presence(CLIENT_ID)
-    presence.connect()
     while True:
         data = get_presence_data()
-        presence.update(**data)
+        RPC.update(**data)
         time.sleep(15)
 
-
-if __name__ in '__main__':
+if __name__ == '__main__':
     run()
